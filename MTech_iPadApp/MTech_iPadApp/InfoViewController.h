c290041d2b377763213c021134f6e9c7da202b05
Tom Longabaugh <tom.longabaugh@nyu.edu>
2015-03-16 16:29:33 -0400

    adding third view controller for department info

---
+++ MTech_iPadApp/MTech_iPadApp/InfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfoViewController.h
+//  MTech_iPadApp
+//
+//  Created by Tom Longabaugh on 3/16/15.
+//  Copyright (c) 2015 nyu.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoViewController : UIViewController
+
+@end
---
+++ MTech_iPadApp/MTech_iPadApp/InfoViewController.m
@@ -0,0 +1,37 @@
+//
+//  InfoViewController.m
+//  MTech_iPadApp
+//
+//  Created by Tom Longabaugh on 3/16/15.
+//  Copyright (c) 2015 nyu.edu. All rights reserved.
+//
+
+#import "InfoViewController.h"
+
+@interface InfoViewController ()
+
+@end
+
+@implementation InfoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
