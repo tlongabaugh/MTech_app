08708bf519f770e5a9469afbaf620959b46fd1a1
Tom Longabaugh <tom.longabaugh@nyu.edu>
2015-04-22 23:14:06 -0400

    change

--- MTech_iPadApp/MTech_iPadApp/CalendarViewController.m
+++ MTech_iPadApp/MTech_iPadApp/CalendarViewController.m
@@ -19,7 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     // Display the Calendar
-    [self displayFFCalendar];
+    //[self displayFFCalendar];
     /*CGRect frame = CGRectMake(0., 0., self.view.frame.size.width, self.view.frame.size.height);
     
     FFMonthCalendarView *viewCalendarYear = [[FFMonthCalendarView alloc] initWithFrame:frame];
